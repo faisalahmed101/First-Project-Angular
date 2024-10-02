812a8a15e560437b40b4f4ef9978007a4458b28c
Faisal <faisalahmed4315@gmail.com>
2024-10-02 12:06:01 +0600

    component creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestComponent } from "./test/test.component";
+import { AboutComponent } from "./componets/about/about.component";
+import { FirstComponent } from "./componets/first/first.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TestComponent, AboutComponent, FirstComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

accc1d2065cd74c2018130b4fc8e4139d861acc4
Faisal <faisalahmed4315@gmail.com>
2024-10-12 01:15:20 +0600

    add route and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { TestComponent } from "./test/test.component";
 import { AboutComponent } from "./componets/about/about.component";
 import { FirstComponent } from "./componets/first/first.component";
+import { ContactusComponent } from "./componets/contactus/contactus.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TestComponent, AboutComponent, FirstComponent],
+  imports: [RouterOutlet, RouterLink, TestComponent, AboutComponent, FirstComponent, ContactusComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'first_project';
+  name = 'Faisal'
+  students = ['faisal', 'ripat', 'fahim']
+
+  changename(){
+    this.name = 'Faisal Ahmed'
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './componets/about/about.component';
+import { HomeComponent } from './componets/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'about', component: AboutComponent}
+];
